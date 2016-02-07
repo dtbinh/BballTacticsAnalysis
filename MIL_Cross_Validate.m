50a9b9d8ba670a2f551fe64c632fd1a31bfc1f31
Peter Tsai <laputianzen@gmail.com>
2016-02-07 21:22:41 +0800

    add capability for Evaluation
    evaluation method to save the MIL SVM method

--- MIL_Cross_Validate.m
+++ MIL_Cross_Validate.m
@@ -18,6 +18,9 @@ num_folder = preprocess.NumCrossFolder;
   run.bag_pred = zeros(num_data, 3);
   run.bag_pred(:, 1) = (1:num_data)';
 
+
+  
+  
 for i = 1:num_folder
   fprintf('Iteration %d ......\n', i);  
   % Generate the data indeces for the testing data
@@ -33,6 +36,47 @@ for i = 1:num_folder
   
   % Classificaiton
   run_class(i) = feval(classifier_wrapper_handle, bags, trainindex, testindex, classifier); 
+  %copyfile('temp/temp.output.txt',[preprocess.WorkingDir '/' strtok(preprocess.input_file,'.') '/cross_validate/' classifier '_iter' int2str(i) '_' classifier '.txt']);
+  %copyfile('temp/temp.output.txt',[strtok(preprocess.input_file,'.') '/cross_validate/' classifier '_iter' int2str(i) '_' classifier '.txt']);
+  k = strfind(preprocess.output_file,'.data.result');
+  testbags = bags(testindex);
+  filename = [preprocess.output_file(1:k-1) '_validate' int2str(i) '.txt'];
+  fid = fopen(filename,'w');
+  fprintf(fid,'bagName  bagPred  bagTruth  bagProb  instPred  instTruth  instProb \n');
+  
+  %Positive = 0;
+  %Negative=  0;
+  truePositive = 0;
+  trueNegative = 0;
+  falsePositive= 0;
+  falseNegative= 0;    
+
+  for j = 1:length(testbags)
+    [num_inst, num_feature] = size(testbags(j).instance);
+    %for j = 1:num_inst
+        %str = [bags(i).name ',' cell2mat(bags(i).inst_name(j)) ',' feature_line(bags(i).instance(j,:)) num2str(bags(i).inst_label(j)) '\n'];
+        str = [testbags(j).name '  ' int2str(run_class(i).bag_label(j)) '  ' int2str(testbags(j).label) '    ' num2str(run_class(i).bag_prob(j),'%.3f') ';  ' ...
+            num2str(run_class(i).inst_label((j-1)*num_inst+1:j*num_inst)','%d') '  ' num2str(testbags(j).inst_label,'%d') '  ' num2str(run_class(i).inst_prob((j-1)*num_inst+1:j*num_inst)','% .3f') '\n'];
+        fprintf(fid, str);
+    %end
+    %Positive = Positive + sum(testbags(j).inst_label);
+    %Negative= Negative + sum(not(testbags(j).inst_label));
+    truePositive = truePositive + sum(and(run_class(i).inst_label((j-1)*num_inst+1:j*num_inst)', testbags(j).inst_label));
+    trueNegative = trueNegative + sum(and(not(run_class(i).inst_label((j-1)*num_inst+1:j*num_inst)'),not(testbags(j).inst_label)));
+    falsePositive= falsePositive + sum(and(not(run_class(i).inst_label((j-1)*num_inst+1:j*num_inst)'),testbags(j).inst_label));
+    falseNegative= falseNegative + sum(and(run_class(i).inst_label((j-1)*num_inst+1:j*num_inst)',not(testbags(j).inst_label)));
+  end
+  
+  Accu = (truePositive+trueNegative)/(truePositive+trueNegative+falsePositive+falseNegative);
+  Prec = truePositive/(truePositive+falsePositive);
+  Reca = truePositive/(truePositive+falseNegative);
+  F1 = 2*truePositive/(2*truePositive+falsePositive+falseNegative);
+  fprintf(fid,'\n');
+  fprintf(fid,'TP TN FP FN Accu Prec Reca F1 \n');
+  str1 = [int2str(truePositive) ' ' int2str(trueNegative) ' ' int2str(falsePositive) ' ' int2str(falseNegative) ' ' num2str(Accu,'%.3f') ' ' num2str(Prec,'%.3f') ' ' num2str(Reca,'%.3f') ' ' num2str(F1,'%.3f')];
+  fprintf(fid, str1);
+  fclose(fid);
+  
   
   run.bag_pred(testindex, 2) = run_class(i).bag_prob; 
   run.bag_pred(testindex, 3) = run_class(i).bag_label; 
--- MIL_Leave_One_Out.m
+++ MIL_Leave_One_Out.m
@@ -17,7 +17,25 @@ for i = 1:num_data
 
     % Classificaiton
     run_class(i) = feval(classifier_wrapper_handle, bags, trainindex, testindex, classifier);
-
+    %copyfile('temp/temp.output.txt',[preprocess.WorkingDir '/' strtok(preprocess.input_file,'.') '_iter' int2str(i) '_' classifier '.txt']);
+    %copyfile('temp/temp.output.txt',[preprocess.WorkingDir '/' strtok(preprocess.input_file,'.') '/leave_one_out/' classifier '_iter' int2str(i) '_' classifier '.txt']);
+    %copyfile('temp/temp.output.txt',[strtok(preprocess.input_file,'.') '/leave_one_out/' classifier '_iter' int2str(i) '_' classifier '.txt']);
+    k = strfind(preprocess.output_file,'.data.result');
+    testbags = bags(testindex);
+    filename = [preprocess.output_file(1:k-1) '_validate' int2str(i) '.txt'];
+    fid = fopen(filename,'w');
+    fprintf(fid,'bagName  bagPred  bagTruth  bagProb  instPred  instTruth instProb \n');
+    for j = 1:length(testbags)
+        [num_inst, num_feature] = size(testbags(j).instance);
+        %for j = 1:num_inst
+        %str = [bags(i).name ',' cell2mat(bags(i).inst_name(j)) ',' feature_line(bags(i).instance(j,:)) num2str(bags(i).inst_label(j)) '\n'];
+        str = [testbags(j).name '  ' int2str(run_class(i).bag_label(j)) '  ' int2str(testbags(j).label) '    ' num2str(run_class(i).bag_prob(j),'%.3f') ';  ' ...
+            num2str(run_class(i).inst_label((j-1)*num_inst+1:j*num_inst)','%d') '  ' num2str(testbags(j).inst_label,'%d') '  ' num2str(run_class(i).inst_prob((j-1)*num_inst+1:j*num_inst)','% .3f') '\n'];
+        fprintf(fid, str);
+        %end
+    end
+    fclose(fid);
+    
     run.bag_pred(testindex, 2) = run_class(i).bag_prob;
     run.bag_pred(testindex, 3) = run_class(i).bag_label;
     run.bag_pred(testindex, 4) = [bags(testindex).label]';
