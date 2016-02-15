c516bc6a7eb686b240bbb31b7e0653f15d5c8484
Peter Tsai <laputianzen@gmail.com>
2016-02-15 11:18:05 +0800

    run pc/cuda svm-gpu-train properly

--- LibSVM.m
+++ LibSVM.m
@@ -20,7 +20,7 @@ end
         
 % set up the commands
 if ispc  && gpuDeviceCount
-    train_cmd = sprintf('!svm-gpu\\svm-train -b 1 -s 0 -t %d %s -c %f -w1 1 -w0 %f %s %s > log1', p(1), s, p(3), p(4), temp_train_file, temp_model_file);
+    train_cmd = sprintf('!svm-gpu\\svm-train-gpu -b 1 -s 0 -t %d %s -c %f -w1 1 -w0 %f %s %s > log1', p(1), s, p(3), p(4), temp_train_file, temp_model_file);
     test_cmd = sprintf('!svm-gpu\\svm-predict -b 1 %s %s %s > log1', temp_test_file, temp_model_file, temp_output_file);
 elseif ispc && ~gpuDeviceCount
     train_cmd = sprintf('!svm\\svmtrain -b 1 -s 0 -t %d %s -c %f -w1 1 -w0 %f %s %s > log1', p(1), s, p(3), p(4), temp_train_file, temp_model_file);
