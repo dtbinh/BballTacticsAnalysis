48fd8035d8ec5afd0239657d3fe33fa2f7b7c8c6
Peter Tsai <laputianzen@gmail.com>
2016-02-24 12:07:54 +0800

    delete accidentally upload tuning folder
    This reverts commit 31cce5735e2aa111cb585f471448871e7b9d760b.

--- showSVMTune.m
+++ showSVMTune.m
@@ -3,31 +3,28 @@ clear all
 
 tactics = {'F23','EV','HK','PD','PT','RB','SP','WS','WV','WW'};
 %features = {'P','V','A','Arc','deltaA','deltaP','Zone','All'};
-%features = {'P','V','A','Arc','deltaA','deltaP','jointAreaIoU'};
+features = {'P','V','A','Arc','deltaA','deltaP','jointAreaIoU'};
 %features = {'Zone'};
-features = {'P'};
 evalOption = {'leave_one_out','cross_validate'};
-%dataset = {'small','large'};
-dataset = {'nonsyncLarge','syncLarge'};
+dataset = {'small','large'};
 svm = {'instMI','bagMI'};
 %targetDir = 'dataResultForMILL';
 %targetDir = 'dataJointArea';
-%targetDir = 'data';
-targetDir = 'tuning';
+targetDir = 'data';
 
-%query = [2 10];
-query = 1:length(tactics);
+query = [2 10];
+
 
 for i=1:length(query)%:length(tactics)
     t = query(i);
-    for d=1:length(dataset)
+    for d=2%:length(dataset)
         for f=1:length(features)
             for s=1:length(svm)
                 fid = figure(1);
                 %figure('name',['tatic ' tactics{t} ',' dataset{d} ',' features{f}]);
                 fid.Name = ['tatic ' tactics{t} ',' dataset{d} ',' features{f}];
-                TuneSVMParam(targetDir,tactics{t},evalOption{2},dataset{d},features{f},svm{s});
-                %pause
+                TuneSVMParam(targetDir,tactics{t},evalOption{d},dataset{d},features{f},svm{s});
+                pause
             end
         end
     end
