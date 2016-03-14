6e705a51cd78782929067dc511e53ce9100cc082
Peter Tsai <laputianzen@gmail.com>
2016-03-14 10:34:14 +0800

    clear unused codes, change tuning folder name order and separate bag_MI and inst_MI execution

--- codes/MILL/BenchResult.m
+++ codes/MILL/BenchResult.m
@@ -1,58 +1,13 @@
-function BenchResult(EvaluationMethod,kernelType) 
+function BenchResult(EvaluationMethod,kernelType,SVMType) 
+
+% eg: EvaluationMethod = 'cross_validate')
+%     kernelType = 'linear','RBF'
+%     SVMType = 'inst_MI','bag_MI'
 %global bagCounter instCounter
-%maxNumCompThreads(1);
-% % Leave one out 
-% subfolder = ['LOUFeature-dist0/' featureSelect];
-% mkdir([subfolder '/WW' featureSelect '/leave_one_out/']);
-% MIL_Run(['classify -t ' subfolder '/WW' featureSelect '.data -o ' ...
-%     subfolder '/WW' featureSelect '/leave_one_out/APR.data.result -p ' ...
-%     subfolder '/WW' featureSelect '/leave_one_out/APR.data.pred -if 0 ¡Vn 1 -distrib 0 -- leave_one_out -- iterdiscrim_APR']);
-% pause
-% MIL_Run(['classify -t ' subfolder '/WW' featureSelect '.data -o ' ...
-%     subfolder '/WW' featureSelect '/leave_one_out/instMI.data.result -p '...
-%     subfolder '/WW' featureSelect '/leave_one_out/instMI.data.pred -if 0 ¡Vn 1 -distrib 0 -- leave_one_out -- inst_MI_SVM -Kernel 2']);
-% %copyfile('temp/temp.output.txt',[featureSelect '/WW' featureSelect '_mi.output.txt']);
-% pause
-% MIL_Run(['classify -t ' subfolder '/WW' featureSelect '.data -o ' ...
-%     subfolder '/WW' featureSelect '/leave_one_out/bagMI.data.result -p ' ...
-%     subfolder '/WW' featureSelect '/leave_one_out/bagMI.data.pred -if 0 ¡Vn 1 -distrib 0 -- leave_one_out -- bag_MI_SVM -Kernel 2']);
-
-% % 3-fold cross validation
-% mkdir([featureSelect '/WW' featureSelect '/cross_validate/']);
-% MIL_Run(['classify -t ' featureSelect '/WW' featureSelect '.data -o ' featureSelect '/WW' featureSelect '/cross_validate/APR.data.result -p ' featureSelect '/WW' featureSelect '/cross_validate/APR.data.pred -if 0 -sf 1 ¡Vn 1 -distrib 0 -- cross_validate -t 3 -- iterdiscrim_APR']);
-% pause
-% MIL_Run(['classify -t ' featureSelect '/WW' featureSelect '.data -o ' featureSelect '/WW' featureSelect '/cross_validate/instMI.data.result -p ' featureSelect '/WW' featureSelect '/cross_validate/instMI.data.pred -if 0 -sf 1 ¡Vn 1 -distrib 0 -- cross_validate -t 3 -- inst_MI_SVM -Kernel 0']);
-% %copyfile('temp/temp.output.txt',[featureSelect '/WW' featureSelect '_mi.output.txt']);
-% pause
-% MIL_Run(['classify -t ' featureSelect '/WW' featureSelect '.data -o ' featureSelect '/WW' featureSelect '/cross_validate/bagMI.data.result -p ' featureSelect '/WW' featureSelect '/cross_validate/bagMI.data.pred -if 0 ¡Vn 1 -distrib 0 -- cross_validate -t 3 -- bag_MI_SVM -Kernel 0']);
-
-% % Test SVM Parameter
-% KernelParamO = 0.05;
-% CostFactorO  = 1;
-% NegativeWeightO = 1;
-% for i = -3:3
-%     for j=-3:3
-%         for k=-3:3
-%             KernelParam = 2^i*KernelParamO
-%             CostFactor  = 2^j*CostFactorO
-%             NegativeWeight=2^k*NegativeWeightO
-% subfolder = [featureSelect '/WW' featureSelect '/leave_one_out/SVM/K=' num2str(KernelParam) 'C=' num2str(CostFactor) 'N=' num2str(NegativeWeight) '/'];
-% mkdir(subfolder);
-% 
-% MIL_Run(['classify -t ' featureSelect '/WW' featureSelect '.data -o ' ... 
-%     subfolder '/instMI.data.result -p ' subfolder '/instMI.data.pred -if 0 ¡Vn 1 -distrib 0 -- leave_one_out -- inst_MI_SVM -Kernel 2 -KernelParam ' ... 
-%     num2str(KernelParam) ' -CostFactor ' num2str(CostFactor) ' -NegativeWeight ' num2str(NegativeWeight)]);
-% %copyfile('temp/temp.output.txt',[featureSelect '/WW' featureSelect '_mi.output.txt']);
-% %pause
-% MIL_Run(['classify -t ' featureSelect '/WW' featureSelect '.data -o ' ...
-%     subfolder '/bagMI.data.result -p ' subfolder '/bagMI.data.pred -if 0 ¡Vn 1 -distrib 0 -- leave_one_out -- bag_MI_SVM -Kernel 2 -KernelParam ' ...
-%     num2str(KernelParam) ' -CostFactor ' num2str(CostFactor) ' -NegativeWeight ' num2str(NegativeWeight)]);
-%         end
-%     end
-% end
 
 % Test SVM Parameter
-[datafile,pathName] = uigetfile('*.data', 'MultiSelect', 'on');
+homePathName = '..';
+[datafile,pathName] = uigetfile([homePathName filesep  'data/*.data'], 'MultiSelect', 'on');
 if iscell(datafile)
     nbfiles = length(datafile);
 elseif datafile ~= 0
@@ -60,47 +15,19 @@ elseif datafile ~= 0
 else
     nbfiles = 0;
 end
+
+display(datafile);
+
 KernelParamO = 0.05;
 CostFactorO  = 1;
 NegativeWeightO = 1;
 
-homePathName = pwd;
-outputPath = strrep(pathName,[homePathName filesep 'data'],'tuning'); 
-% for f=1:nbfiles
-%     tic
-% for i = -3:3
-%     for j=-3:3
-%         for k=0
-%             KernelParam = 2^i*KernelParamO
-%             CostFactor  = 2^j*CostFactorO
-%             NegativeWeight=2^k*NegativeWeightO
-% if iscell(datafile)
-% subfolder = [pathName '/' strtok(datafile{f},'.') '/leave_one_out/SVM/K=' num2str(KernelParam) 'C=' num2str(CostFactor) 'N=' num2str(NegativeWeight) '/'];
-% inputfile = [pathName datafile{f}];
-% else
-% subfolder = [pathName '/' strtok(datafile,'.') '/leave_one_out/SVM/K=' num2str(KernelParam) 'C=' num2str(CostFactor) 'N=' num2str(NegativeWeight) '/'];
-% inputfile = [pathName datafile];
-% end
-% 
-% mkdir(subfolder);
-% 
-% 
-% MIL_Run(['classify -t ' inputfile ' -o ' ... 
-%     subfolder '/instMI.data.result -p ' subfolder '/instMI.data.pred -if 0 ¡Vn 1 -distrib 0 -- leave_one_out -- inst_MI_SVM -Kernel 2 -KernelParam ' ... 
-%     num2str(KernelParam) ' -CostFactor ' num2str(CostFactor) ' -NegativeWeight ' num2str(NegativeWeight)]);
-% %copyfile('temp/temp.output.txt',[featureSelect '/WW' featureSelect '_mi.output.txt']);
-% %pause
-% MIL_Run(['classify -t ' inputfile ' -o ' ...
-%     subfolder '/bagMI.data.result -p ' subfolder '/bagMI.data.pred -if 0 ¡Vn 1 -distrib 0 -- leave_one_out -- bag_MI_SVM -Kernel 2 -KernelParam ' ...
-%     num2str(KernelParam) ' -CostFactor ' num2str(CostFactor) ' -NegativeWeight ' num2str(NegativeWeight)]);
-%         end
-%     end
-% end
-% toc
-% %pause
-% 
-% end
-% Test SVM Parameter
+while ~exist('normalization','var') || (~strcmp(normalization,'1') && ~strcmp(normalization,'0'))
+    normalization = input('Q: Need Normalization?(Dist:No [type 0], Other:Yes [type 1])','s');
+end
+
+
+outputPath = strrep(pathName,'data','tuning'); 
 
 for f=1:nbfiles
     tic
@@ -116,20 +43,22 @@ end
 switch kernelType
     case 'linear'
     	if iscell(datafile)
-			subfolder = [outputPath strtok(datafile{f},'.') '/' EvaluationMethod '/' kernelType '/SVM/'];
+			subfolder = [outputPath strtok(datafile{f},'.') '/' EvaluationMethod '/SVM/' kernelType '/' ];
 			inputfile = [pathName datafile{f}];
 		else
-			subfolder = [outputPath strtok(datafile,'.') '/' EvaluationMethod '/' kernelType '/SVM/'];
+			subfolder = [outputPath strtok(datafile,'.') '/' EvaluationMethod '/SVM/' kernelType  '/'];
 			inputfile = [pathName datafile];
 		end
 
 		mkdir(subfolder);
 
-	    MIL_Run(['classify -t ' inputfile ' -o ' ... 
-	        subfolder 'instMI.data.result -p ' subfolder 'instMI.data.pred -if 0 ¡Vn 1 -distrib 0 ' EvalCmd ' -- inst_MI_SVM -Kernel 0']);
-
-	    MIL_Run(['classify -t ' inputfile ' -o ' ...
-	        subfolder 'bagMI.data.result -p ' subfolder 'bagMI.data.pred --if 0 ¡Vn 1 -distrib 0 ' EvalCmd ' -- bag_MI_SVM -Kernel 0']);
+% 	    MIL_Run(['classify -t ' inputfile ' -o ' ... 
+% 	        subfolder 'instMI.data.result -p ' subfolder 'instMI.data.pred -if 0 ¡Vn 1 -distrib 0 ' EvalCmd ' -- inst_MI_SVM -Kernel 0']);
+% 
+% 	    MIL_Run(['classify -t ' inputfile ' -o ' ...
+% 	        subfolder 'bagMI.data.result -p ' subfolder 'bagMI.data.pred --if 0 ¡Vn 1 -distrib 0 ' EvalCmd ' -- bag_MI_SVM -Kernel 0']);
+ 	    MIL_Run(['classify -t ' inputfile ' -o ' ... 
+	        subfolder SVMType '.data.result -p ' subfolder SVMType '.data.pred -if 0 ¡Vn ' normalization ' -distrib 0 ' EvalCmd ' -- ' SVMType '_SVM -Kernel 0']);
     case 'RBF'
 		for i = -3:3
 		    for j=-3:3
@@ -138,20 +67,23 @@ switch kernelType
             		CostFactor  = 2^j*CostFactorO
             		NegativeWeight=2^k*NegativeWeightO
 					if iscell(datafile)
-						subfolder = [outputPath strtok(datafile{f},'.') '/' EvaluationMethod '/' kernelType '/SVM/K=' num2str(KernelParam) 'C=' num2str(CostFactor) 'N=' num2str(NegativeWeight) '/'];
+						subfolder = [outputPath strtok(datafile{f},'.') '/' EvaluationMethod  '/SVM/' kernelType '/K=' num2str(KernelParam) 'C=' num2str(CostFactor) 'N=' num2str(NegativeWeight) '/'];
 						inputfile = [pathName datafile{f}];
 					else
-						subfolder = [outputPath strtok(datafile,'.') '/' EvaluationMethod '/' kernelType '/SVM/K=' num2str(KernelParam) 'C=' num2str(CostFactor) 'N=' num2str(NegativeWeight) '/'];
+						subfolder = [outputPath strtok(datafile,'.') '/' EvaluationMethod '/SVM/' kernelType '/K=' num2str(KernelParam) 'C=' num2str(CostFactor) 'N=' num2str(NegativeWeight) '/'];
 						inputfile = [pathName datafile];
 					end
 
 					mkdir(subfolder);    
-        			MIL_Run(['classify -t ' inputfile ' -o ' ... 
-            			subfolder 'instMI.data.result -p ' subfolder 'instMI.data.pred -if 0 ¡Vn 1 -distrib 0 ' EvalCmd ' -- inst_MI_SVM -Kernel 2 -KernelParam ' ... 
-            			num2str(KernelParam) ' -CostFactor ' num2str(CostFactor) ' -NegativeWeight ' num2str(NegativeWeight)]);
-        
+%         			MIL_Run(['classify -t ' inputfile ' -o ' ... 
+%             			subfolder 'instMI.data.result -p ' subfolder 'instMI.data.pred -if 0 ¡Vn 0 -distrib 0 ' EvalCmd ' -- inst_MI_SVM -Kernel 2 -KernelParam ' ... 
+%             			num2str(KernelParam) ' -CostFactor ' num2str(CostFactor) ' -NegativeWeight ' num2str(NegativeWeight)]);
+%         
+%         			MIL_Run(['classify -t ' inputfile ' -o ' ...
+%             			subfolder 'bagMI.data.result -p ' subfolder 'bagMI.data.pred --if 0 ¡Vn 0 -distrib 0 ' EvalCmd ' -- bag_MI_SVM -Kernel 2 -KernelParam ' ...
+%             			num2str(KernelParam) ' -CostFactor ' num2str(CostFactor) ' -NegativeWeight ' num2str(NegativeWeight)]);
         			MIL_Run(['classify -t ' inputfile ' -o ' ...
-            			subfolder 'bagMI.data.result -p ' subfolder 'bagMI.data.pred --if 0 ¡Vn 1 -distrib 0 ' EvalCmd ' -- bag_MI_SVM -Kernel 2 -KernelParam ' ...
+            			subfolder SVMType '.data.result -p ' subfolder SVMType '.data.pred --if 0 ¡Vn ' normalization ' -distrib 0 ' EvalCmd ' -- ' SVMType '_SVM -Kernel 2 -KernelParam ' ...
             			num2str(KernelParam) ' -CostFactor ' num2str(CostFactor) ' -NegativeWeight ' num2str(NegativeWeight)]);
          			end
     			end
