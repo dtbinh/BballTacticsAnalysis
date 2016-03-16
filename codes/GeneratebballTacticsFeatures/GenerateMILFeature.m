ea12aa8856a2fffa3c7452d7cac478569f673d14
Peter Tsai <laputianzen@gmail.com>
2016-03-16 22:35:55 +0800

    batch running mil feature generation

---
+++ codes/GeneratebballTacticsFeatures/GenerateMILFeature.m
@@ -0,0 +1,30 @@
+function GenerateMILFeature(playerNum,featureName,featureData,tactics,syncFlag,mKeyPlayerIndex)
+
+if playerNum ~= 1
+    playerFolder = 'multiPlayers';
+else
+    playerFolder = 'singlePlayer';
+end
+if ~exist('syncFlag','var') || syncFlag
+    syncFlag = 1;
+    syncPrefix = 'sync';
+else
+    syncPrefix = 'nonSync';
+end
+   
+
+largeDataPathPrefix = [pwd '/data/' playerFolder '/' syncPrefix 'Large'];
+if ~exist('mKeyPlayerIndex','var')
+    for f = 1:length(featureName)
+        largeDataPath = [largeDataPathPrefix featureName{f}];
+        saveMILLFeatureData(tactics,featureData{f},largeDataPath,featureName{f});
+    end
+else
+    for f = 1:length(featureName)
+        largeDataPath = [largeDataPathPrefix featureName{f}];
+        saveMILLFeatureData(tactics,featureData{f},largeDataPath,[featureName{f} int2str(playerNum)],mKeyPlayerIndex);
+    end
+end
+
+
+end
---
+++ runMILFeature.m
@@ -0,0 +1,10 @@
+
+clear all
+close all
+
+addpath(genpath([pwd '/codes/GeneratebballTacticsFeatures']));
+
+gMIL('codes/GeneratebballTacticsFeatures');
+
+
+rmpath(genpath([pwd '/codes/GeneratebballTacticsFeatures']));
