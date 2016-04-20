f61625e8422eac5771d461982e8a537d39ee2321
Peter Tsai <laputianzen@gmail.com>
2016-04-21 00:44:46 +0800

    change convert to support new param and iteration scheme

--- runConvert.m
+++ runConvert.m
@@ -6,26 +6,38 @@ clear all
 targetDir = 'tuning/multiPlayers';
 tacticSelect = {'F23','EV','HK','PD','PT','RB','SP','WS','WV','WW'};
 datasetSelect = 'syncLarge';
-SVMType = 'bag_MI'; %inst_MI
+SVMType = 'inst_MI'; %inst_MI
 EvaluationSelect = 'cross_validate';
-featureSelect = 'ZoneDist';
+featureSelect = 'ZoneVelocitySoftAssign';%'ZoneDist';
 SVMKernelType = 'RBF';
-playerNum = {'3','3','3','3','3','3','2','3','5','2'};
+playerNum = {'3','3','3','3','4','3','2','3','5','2'};
 
-KernelParamO = 1;
+param.kernel = 5:-1:-5;
+param.cost = 10:-1:-5;
+param.negativeWeight = 0;
+param.iter = 2;
+
+KernelParamO = 1/1040;
 CostFactorO  = 1;
-NegativeWeightO = 1;
-for t = 1:length(tacticSelect)
-for i = -3:3
-    for j=-3:3
-        for k=0
+NegativeWeight0 = 1;
+
+for t = 2%1:length(tacticSelect)
+    
+for i = param.kernel
+    
+    for j= param.cost
+        for k=param.negativeWeight
+            for iter = 1:param.iter
             KernelParam = 2^i*KernelParamO;
             CostFactor  = 2^j*CostFactorO;
-            NegativeWeight=2^k*NegativeWeightO;
+            NegativeWeight=2^k* NegativeWeight0;
             if ~isempty(playerNum{t})
                 subfolder = [targetDir '/' datasetSelect featureSelect '/' tacticSelect{t} featureSelect playerNum{t} '/' EvaluationSelect '/SVM/' SVMType '/' SVMKernelType '/K=' num2str(KernelParam) 'C=' num2str(CostFactor) 'N=' num2str(NegativeWeight)];
             else
                 subfolder = [targetDir '/' datasetSelect featureSelect '/' tacticSelect{t} featureSelect '/' EvaluationSelect '/SVM/' SVMType '/' SVMKernelType '/K=' num2str(KernelParam) 'C=' num2str(CostFactor) 'N=' num2str(NegativeWeight)];
+            end
+            if param.iter ~= 1
+                subfolder = [subfolder '/iter' int2str(iter)];
             end
 
             trainingfileLists = dir([subfolder '/' SVMType '_training*']);
@@ -47,6 +59,7 @@ for i = -3:3
             fid = fopen(resultFile,'w');
             fprintf(fid, str);
             fclose(fid);
+            end
         end
     end
 end
