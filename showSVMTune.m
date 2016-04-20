5f68e5c412b862bcd002fe49e1038a9f15244a14
Peter Tsai <laputianzen@gmail.com>
2016-04-21 00:48:32 +0800

    fix showSVMTune.m

--- showSVMTune.m
+++ showSVMTune.m
@@ -15,21 +15,22 @@ dataset = {'nonSyncLarge','syncLarge'};
 svm = {'inst_MI','bag_MI'};
 SVMKernelType = 'RBF'; % RBF,linear
 
-targetDir = 'tuningNEW';
+targetDir = 'tuning';
 
 % multiple players setting
 % group = 'multiPlayers/Convert(Th)';
-% playerNum = {'3','3','3','3','3','3','2','3','5','2'};
+% playerNum = {'3','3','3','3','4','3','2','3','5','2'};
 
 % % % single player setting
 group = 'singlePlayer';
 % playerNum = {'1','1','1','1','1','1','1','1','1','1'};
 playerNum = {'','','','','','','','','',''};
 
-param.kernel0 = 1;
 
-param.kernel = 3:-1:-15;
-param.cost = 15:-1:-5;
+param.kernel0 = 1; %default is 1 (or 1/num_feature)
+
+param.kernel = 5:-1:-5;   % default 3:-1:-15;
+param.cost = 10:-1:-5;  %default 15:-1:-5;
 param.negativeWeight = 0;
 
 
@@ -43,7 +44,7 @@ for i=1:length(query)%:length(tactics)
                 fid = figure(1);
                 %figure('name',['tatic ' tactics{t} ',' dataset{d} ',' features{f}]);
                 fid.Name = ['tatic ' tactics{t} ',' dataset{d} ',' features{f}];
-                TuneSVMParam([targetDir '/' group],playerNum{i},tactics{t},evalOption{d},dataset{d},features{f},svm{s},SVMKernelType);
+                TuneSVMParam(param,[targetDir '/' group],playerNum{i},tactics{t},evalOption{d},dataset{d},features{f},svm{s},SVMKernelType);
                 pause
             end
         end
