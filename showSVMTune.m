21ec77cf5eb7ab7e106eae3e4b492b4b12803e7a
Peter Tsai <laputianzen@gmail.com>
2016-04-24 11:43:36 +0800

    clear old code and support automatic kernel param in showSVMTune

--- showSVMTune.m
+++ showSVMTune.m
@@ -1,33 +1,37 @@
 close all
 clear all
+addpath(genpath([pwd '/codes/MILL']));
 
 tactics = {'F23','EV','HK','PD','PT','RB','SP','WS','WV','WW'};
-%features = {'P','V','A','Arc','deltaA','deltaP','Zone','All'};
-%features = {'P','V','A','Arc','deltaA','deltaP','jointAreaIoU'};
-%features = {'Zone','ZoneV'};
-%features = {'P+V'}%,'V'};
-%features = {'ZoneDist'};
+
+% features = {'ZoneDist'};
+% features = {'ZoneSoftAssignDist'};
+% features = {'ZoneVelocitySoftAssign'};
 features = {'P','V'};
-%features = {'Zone'}
+% features = {'Zone'}
 evalOption = {'leave_one_out','cross_validate'};
-%dataset = {'small','large'};
 dataset = {'nonSyncLarge','syncLarge'};
 svm = {'inst_MI','bag_MI'};
 SVMKernelType = 'RBF'; % RBF,linear
 
-targetDir = 'tuning';
+targetDir = 'tuningUltimate';
+dataDir = 'dataUltimate';
 
-% multiple players setting
-% group = 'multiPlayers/Convert(Th)';
+% % multiple players setting
+% group = 'multiPlayers';
+% Converted = 'Convert(Th)';
 % playerNum = {'3','3','3','3','4','3','2','3','5','2'};
 
-% % % single player setting
+% % single player setting
 group = 'singlePlayer';
-% playerNum = {'1','1','1','1','1','1','1','1','1','1'};
-playerNum = {'','','','','','','','','',''};
+Converted ='';
+if strfind(features{1},'Zone')
+    playerNum = {'1','1','1','1','1','1','1','1','1','1'};
+else    
+    playerNum = {'','','','','','','','','',''};
+end
 
 
-param.kernel0 = 1; %default is 1 (or 1/num_feature)
 
 param.kernel = 5:-1:-5;   % default 3:-1:-15;
 param.cost = 10:-1:-5;  %default 15:-1:-5;
@@ -44,9 +48,24 @@ for i=1:length(query)%:length(tactics)
                 fid = figure(1);
                 %figure('name',['tatic ' tactics{t} ',' dataset{d} ',' features{f}]);
                 fid.Name = ['tatic ' tactics{t} ',' dataset{d} ',' features{f}];
-                TuneSVMParam(param,[targetDir '/' group],playerNum{i},tactics{t},evalOption{d},dataset{d},features{f},svm{s},SVMKernelType);
-                pause
+                
+                inputfile = [dataDir '/' group '/' dataset{d} features{f} '/' tactics{t} features{f} playerNum{i} '.data'];
+                global preprocess
+                preprocess.InputFormat = 0;
+                preprocess.Normalization = 0;
+                preprocess.Shuffled = 0;
+                [~, ~, num_feature] = MIL_Data_Load(inputfile);
+                % num_feature
+                % pause(0.5)
+                clear preprocess
+                param.kernel0 = 1/num_feature; %default is 1 (or 1/num_feature)
+                
+                
+                TuneSVMParam(param,[targetDir '/' group '/' Converted],playerNum{i},tactics{t},evalOption{d},dataset{d},features{f},svm{s},SVMKernelType);
+                pause(5)
             end
         end
     end
 end
+
+rmpath(genpath([pwd '/codes/MILL']));
