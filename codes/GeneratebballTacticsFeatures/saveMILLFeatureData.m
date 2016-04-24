41978cb189fda385dbf09b84c129d2db074872a0
Peter Tsai <laputianzen@gmail.com>
2016-04-24 21:56:26 +0800

    support new velocity soft assign concatenation

--- codes/GeneratebballTacticsFeatures/saveMILLFeatureData.m
+++ codes/GeneratebballTacticsFeatures/saveMILLFeatureData.m
@@ -1,16 +1,31 @@
 function saveMILLFeatureData(tactics,assign,MILLFeatureData,DataPath,featureName,mKeyPlayerIndex)
 
 for ref = 1:length(tactics.refVideoIndex)
-    %largeDataPath = ['../data/single/syncLargeP'];
     MILLFeatureDataAlign = InterTacticPlayerAlign(MILLFeatureData,tactics,assign,ref);
-    
-    if ~strcmp(featureName(end),'1') && ~isempty(strfind(featureName(1:end-1),'Zone'))
-        if str2num(featureName(end)) == sum(mKeyPlayerIndex(tactics.refVideoIndex(ref),:)) % keyplayer number equal to featureIndex
-            [MILLFeatureDataAlign,mKeyPlayerIndexCombined] = MultiplePlayerFeature(MILLFeatureDataAlign,tactics.keyPlayer,str2num(featureName(end)));
-        else
-            continue
+    if isempty(strfind(featureName,'Concat'))
+        %largeDataPath = ['../data/single/syncLargeP'];
+
+        if ~strcmp(featureName(end),'1') && ~isempty(strfind(featureName(1:end-1),'Zone'))
+            if str2num(featureName(end)) == sum(mKeyPlayerIndex(tactics.refVideoIndex(ref),:)) % keyplayer number equal to featureIndex
+                [MILLFeatureDataFinal,mKeyPlayerIndexCombined] = MultiplePlayerFeature(MILLFeatureDataAlign,tactics.keyPlayer,str2num(featureName(end)));
+            else
+                continue
+            end
+        end
+     else
+        MILLFeatureDataFinal = [];
+        mKeyPlayerIndexCombined =[];
+        for i = 2:size(mKeyPlayerIndex,2)
+            [MILLFeatureDataAlignTemp,mKeyPlayerIndexTemp] =  MultiplePlayerFeature(MILLFeatureDataAlign,tactics.keyPlayer,i);
+            MILLFeatureDataFinal = [MILLFeatureDataFinal MILLFeatureDataAlignTemp];
+            if i ~= sum(mKeyPlayerIndex(tactics.refVideoIndex(ref),:))
+                mKeyPlayerIndexCombined = [mKeyPlayerIndexCombined zeros(size(mKeyPlayerIndexTemp))];
+            else
+                mKeyPlayerIndexCombined = [mKeyPlayerIndexCombined mKeyPlayerIndexTemp];
+            end
         end
     end
+    
     mkdir(DataPath);
     nonTacticIndex = tactics.videoIndex;
     nonTacticIndex{ref} = [];
@@ -19,8 +34,9 @@ for ref = 1:length(tactics.refVideoIndex)
     if ~exist('mKeyPlayerIndex','var') || strcmp(featureName(end),'1')
         CreateBags(DataPath,featureName,MILLFeatureDataAlign,tactics.Name{ref},tacticIndex,nonTacticIndex,tactics.keyPlayer(tacticIndex,:));
     else
-        CreateBags(DataPath,featureName,MILLFeatureDataAlign,tactics.Name{ref},tacticIndex,nonTacticIndex,mKeyPlayerIndexCombined(tacticIndex,:));
+        CreateBags(DataPath,featureName,MILLFeatureDataFinal,tactics.Name{ref},tacticIndex,nonTacticIndex,mKeyPlayerIndexCombined(tacticIndex,:));
     end
+
 end
 
 end
