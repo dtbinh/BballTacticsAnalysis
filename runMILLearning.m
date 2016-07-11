2d5329ecabaef047db39781053ce48fc969c318a
Peter Tsai <laputianzen@gmail.com>
2016-07-11 23:58:17 +0800

    new BatchTacticOnce mode added in MIL Learning file

--- runMILLearning.m
+++ runMILLearning.m
@@ -1,13 +1,16 @@
 clear all
 
 addpath(genpath([pwd '/codes/MILL']));
+addpath(genpath([pwd '/codes/GeneratebballTacticsFeatures']));
 
 param.kernel = 5:-1:-5;
 param.cost = 10:-1:-5;
 param.negativeWeight = 0;
 param.iter = 1; %default is 1
 
-BenchResult(param,'cross_validate','RBF','inst_MI');
+BatchAllTacticOnce(param,'cross_validate','RBF','inst_MI','Training');
+
+% BenchResult(param,'cross_validate','RBF','inst_MI');
 
 % BenchResult(param,'cross_validate','RBF','bag_MI');
 
@@ -16,3 +19,6 @@ BenchResult(param,'cross_validate','RBF','inst_MI');
 % BenchResult(param,'cross_validate','linear','bag_MI');
 
 rmpath(genpath([pwd '/codes/MILL']));
+rmpath(genpath([pwd '/codes/GeneratebballTacticsFeatures']));
+
+%rmdir('tmp','s');
